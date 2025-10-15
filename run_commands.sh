00c0ad09d5a7c78110c2140120dd902a685663c3
TaiMingLu <terrytaimingl@gmail.com>
2025-10-15 04:17:49 -0400

    sync env

--- run_commands.sh
+++ run_commands.sh
@@ -26,4 +26,18 @@ gcloud compute tpus tpu-vm ssh terry@${TPU_NAME} \
   --project=${PROJECT_ID} --zone=${ZONE} \
   --worker=all \
   --ssh-key-file="~/.ssh/id_rsa" \
-  --command='source ~/vision_env/bin/activate && cd ~/vision && python tools/test_tfds_loader.py --data-dir /home/terry/gcs-bucket/Distillation/imagenet_tfds --split train --num-samples 8 '
+  --command='source ~/vision_env/bin/activate && cd ~/vision && python tools/test_tfds_loader.py --data-dir /home/terry/gcs-bucket/Distillation/imagenet_tfds --split train --num-samples 8 --num-workers 8'
+
+
+gcloud compute tpus tpu-vm ssh terry@${TPU_NAME} \
+  --project=${PROJECT_ID} --zone=${ZONE} \
+  --worker=all \
+  --ssh-key-file="~/.ssh/id_rsa" \
+  --command='rm -rf ~/vision && git clone https://github.com/TaiMingLu/vision_transformers_torch_xla.git ~/vision'
+  
+gcloud compute tpus tpu-vm ssh terry@${TPU_NAME} \
+  --project=${PROJECT_ID} --zone=${ZONE} \
+  --worker=all \
+  --ssh-key-file="~/.ssh/id_rsa" \
+  --command='source ~/vision_env/bin/activate && python -m pip install datasets'
+
