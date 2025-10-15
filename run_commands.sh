1c2084350348e8f2d02d6e14f45e32e9a5e2db39
TaiMingLu <terrytaimingl@gmail.com>
2025-10-15 03:43:45 -0400

    sync

--- run_commands.sh
+++ run_commands.sh
@@ -2,8 +2,28 @@ TPU_NAME="taiming-v4-64_000036"
 ZONE="us-central2-b"
 PROJECT_ID="vision-mix"
 
-# Run a command on EVERY TPU host in the node/slice
-gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
+chmod 600 ~/.ssh/id_rsa
+
+gcloud compute tpus tpu-vm ssh terry@${TPU_NAME} \
   --project=${PROJECT_ID} --zone=${ZONE} \
   --worker=all \
-  --command='whoami'
+  --ssh-key-file="~/.ssh/id_rsa" \
+  --command='whoami && ls'
+
+gcloud compute tpus tpu-vm ssh terry@${TPU_NAME} \
+  --project=${PROJECT_ID} --zone=${ZONE} \
+  --worker=all \
+  --ssh-key-file="~/.ssh/id_rsa" \
+  --command='rm -rf ~/vision_env'
+
+gcloud compute tpus tpu-vm ssh terry@${TPU_NAME} \
+  --project=${PROJECT_ID} --zone=${ZONE} \
+  --worker=all \
+  --ssh-key-file="~/.ssh/id_rsa" \
+  --command='rm -rf ~/vision && git clone https://github.com/TaiMingLu/vision_transformers_torch_xla.git ~/vision'
+
+gcloud compute tpus tpu-vm ssh terry@${TPU_NAME} \
+  --project=${PROJECT_ID} --zone=${ZONE} \
+  --worker=all \
+  --ssh-key-file="~/.ssh/id_rsa" \
+  --command='python3 -m venv ~/vision_env && source ~/vision_env/bin/activate && python -m pip install -r /home/terry/vision/requirements.txt'
